4e6fee4ef61fdd80042fb05ea05038d221f8eac4
Dion Lopes <dion.mello@hotmail.com>
2019-06-24 22:39:31 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [{ id: 2, title: 'Projeto 2', tasks: [] }];
+var count = 0;
+
+
+// Middlewares
+
+server.use((req, res, next) => {
+	
+	count++;
+
+	console.log(`Total de requisições ${count}`);
+
+	return next();
+})
+
+function checkProjectExists(req, res, next) {
+	const { id } = req.params;
+
+	project = projects.find(project => project.id == id);
+
+	if (!project) {
+		return res.status(400).send({ message: 'Project does not exists'});
+	}
+
+	return next();
+}
+
+// Routes Project
+server.get('/projects', (req, res) => {
+	return res.json(projects);
+})
+
+server.post('/projects', (req, res) => {
+	const { id, title, tasks } = req.body;
+
+	let project = { 
+		id,
+		title,
+		tasks
+	}
+
+	projects.push(project);
+
+	return res.json(projects);
+})
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+	const { id } = req.params;
+	const { title } = req.body;
+	
+	const project = projects.find(project => project.id == id);
+
+	project.title = title;
+
+	return res.json(projects);
+})
+
+server.delete('/projects/:id', (req, res) => {
+	const { id } = req.params;
+
+	projectIndex = projects.findIndex(project => project.id == id);
+
+	projects.splice(projectIndex, 1);
+
+	return res.send();
+})
+
+// Routes Project + Tasks
+server.put('/projects/:id/tasks', (req, res) => {
+	const { id } = req.params;
+	const { title, tasks } =  req.body;
+
+	const project = projects.find(project => project.id == id);
+
+	project.title = title;
+	project.tasks.push(tasks);
+
+	return res.json(projects);
+
+})
+
+
+
+server.listen(3000);
